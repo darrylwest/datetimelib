85f7fee438694ade64701fce2be158178784e657
darryl west <darryl.west@raincitysoftware.com>
2025-03-04 09:58:01 -0800

    clean ups

--- include/datetimelib/datetimelib.hpp
+++ include/datetimelib/datetimelib.hpp
@@ -6,8 +6,6 @@
 
 #include <chrono>
 #include <functional>
-#include <iomanip>
-#include <sstream>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -22,7 +20,7 @@ namespace datetimelib {
     template <typename K, typename V> using HashMap = std::unordered_map<K, V>;
 
     // the lib version
-    constexpr StrView VERSION = "0.6.1-129";
+    constexpr StrView VERSION = "0.6.1-130";
 
     // return the lib's version
     StrView get_version();
@@ -37,15 +35,15 @@ namespace datetimelib {
     std::time_t timestamp_millis();
 
     // convert the unix timestamp to the local iso date
-    Str ts_to_local_isodate(const std::time_t timestamp, const char *format = "%FT%T%z");
+    Str ts_to_local_isodate(std::time_t timestamp, const char *format = "%FT%T%z");
 
     // convert the unix timestamp to the utc/zulu iso date
-    Str ts_to_utc_isodate(const std::time_t timestamp, const char *format = "%FT%TZ");
+    Str ts_to_utc_isodate(std::time_t timestamp, const char *format = "%FT%TZ");
 
     // parse the datetimme string (iso8601) to a 12 character yyyymmddhhmm
     const Str parse_datetime_to_minutes(const Str &datetime);
 
-    void sleep_seconds(const int seconds, const bool verbose = false);
+    void sleep_seconds(int seconds, bool verbose = false);
 
     // Type alias for a function that provides the current time
     struct MarkProvider {
@@ -57,6 +55,7 @@ namespace datetimelib {
 
     // Function to wait for the next 5-minute interval with a tolerance of +20
     // seconds
-    void wait_for_next_mark(const MarkProvider &provider = MarkProvider{}, const bool verbose = false);
+    void wait_for_next_mark(const MarkProvider &provider = MarkProvider{},
+                            bool verbose = false);
 
 }  // namespace datetimelib
